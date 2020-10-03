3a21eeef1cc4003fa96b474e46a8d7ffa949b21d
phiamo <phiamo@gmail.com>
2020-10-03 13:27:01 +0200

    removing auto installed unused dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -8,6 +8,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
